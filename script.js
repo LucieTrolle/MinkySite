ea4da10991549954fe14088c212a07891efdc07c
Lucie Trolle <lucie.trolle@etu.minesparis.psl.eu>
2024-03-30 16:58:34 +0100

    simplification des fichiers

---
+++ script.js
@@ -0,0 +1,17 @@
+
+function go_petit_dej() {
+    window.location.href = "../html/petit_dej.html";
+    }
+
+function go_crepes() {
+    window.location.href = "../html/crepes.html";
+    }
+
+function go_barbe_a_papa() {
+    window.location.href = "../html/barbe_a_papa.html";
+    }
+
+function retourHotlines() {
+    window.location.href = "../html/hotlines.html";
+    }
+    
