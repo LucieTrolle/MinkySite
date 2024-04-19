d1a1a396fa664a4f4f494409d59a8ce32aa409bb
Lucie Trolle <lucie.trolle@etu.minesparis.psl.eu>
2024-04-19 14:44:20 +0200

    test

--- script.js
+++ script.js
@@ -14,4 +14,62 @@ function go_barbe_a_papa() {
 function retourHotlines() {
     window.location.href = "../html/hotlines.html";
     }
-    
+
+function ValidationPetitDej() {
+    console.log("Fonction ValidationPetitDej appelée."); // Ajout d'un message pour vérifier si la fonction est appelée
+
+    // Récupération des réponses au formulaire
+    var boisson = document.getElementById('boissonP').value;
+    var lieuLivraison = document.getElementById('lieu_livraisonP').value;
+    var jourLivraison = document.getElementById('jour_livraisonP').value;
+    var heureLivraison = document.getElementById('heure-livraisonP').value;
+
+    console.log("Boisson sélectionnée:", boisson); // Ajout d'un message pour vérifier la boisson sélectionnée
+    console.log("Lieu de livraison saisi:", lieuLivraison); // Ajout d'un message pour vérifier le lieu de livraison saisi
+    console.log("Jour de livraison sélectionné:", jourLivraison); // Ajout d'un message pour vérifier le jour de livraison sélectionné
+    console.log("Heure de livraison saisie:", heureLivraison); // Ajout d'un message pour vérifier l'heure de livraison saisie
+
+    // Vérification si le lieu de commande est un entier
+    if (!isNaN(parseInt(lieuLivraison))) {
+        // Si le lieu de commande est un entier, renvoie sur la page 1
+        console.log("Le lieu de livraison est un entier. Redirection vers la page de validation.");
+        window.location.href = '../html/validation_commande.html';
+    } else {
+        // Si le lieu de commande n'est pas un entier, renvoie sur la page 2
+        console.log("Le lieu de livraison n'est pas un entier. Redirection vers la page d'indisponibilité de livraison.");
+        window.location.href = '../html/heure_livraison_indisponible.html';
+    }
+}
+
+function ValidationCrepes() {
+    // Récupération des réponses au formulaire
+    var topping = document.getElementById('toppingC').value;
+    var lieuLivraison = document.getElementById('lieu_livraisonC').value;
+    var jourLivraison = document.getElementById('jour_livraisonC').value;
+    var heureLivraison = document.getElementById('heure-livraisonC').value;
+
+    // Vérification si le lieu de commande est un entier
+    if (!isNaN(parseInt(lieuLivraison))) {
+        // Si le lieu de commande est un entier, renvoie sur la page 1
+        window.location.href = '../html/validation_commande.html';
+    } else {
+        // Si le lieu de commande n'est pas un entier, renvoie sur la page 2
+        window.location.href = '../html/heure_livraison_indisponible.html';
+    }
+}
+
+function ValidationBarbeaPapa() {
+    // Récupération des réponses au formulaire
+    var lieuLivraison = document.getElementById('lieu_livraisonB').value;
+    var jourLivraison = document.getElementById('jour_livraisonB').value;
+    var heureLivraison = document.getElementById('heure-livraisonB').value;
+
+    // Vérification si le lieu de commande est un entier
+    if (!isNaN(parseInt(lieuLivraison))) {
+        // Si le lieu de commande est un entier, renvoie sur la page 1
+        window.location.href = '../html/validation_commande.html';
+    } else {
+        // Si le lieu de commande n'est pas un entier, renvoie sur la page 2
+        window.location.href = '../html/heure_livraison_indisponible.html';
+    }
+}
